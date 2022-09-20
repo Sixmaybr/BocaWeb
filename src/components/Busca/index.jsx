642a066147bb62d8d7246b0796123d478d38661b
Sixmaybr <gabriel.machado5@estudante.ifms.edu.br>
2022-09-20 17:11:24 -0400

    BocaWeb

---
+++ src/components/Busca/index.jsx
@@ -0,0 +1,17 @@
+export const Busca = ({busca, buscaODA}) =>{
+    return (
+        <div className='busca'>
+          <body>
+              <input
+                 name='busca'
+                 type='text'
+                 value={busca}
+                 placeholder='O que deseja buscar?'
+                 onChange={buscaODA}
+              />
+          </body>
+      </div>
+    )
+}
+
+export default Busca
---
+++ src/components/Rodape/index.jsx
@@ -0,0 +1,13 @@
+export const Rodape = ()=>{
+    return(
+        <div className="rodape">
+          <body>
+            <h1>Instituto Federal Mato Grosso do Sul: DOURADOS-MS</h1>
+            <h2> Trabalho de Frameworks de Gabriel da Silva Machado </h2>
+          </body>
+        </div>
+
+    )
+} 
+
+export default Rodape;
